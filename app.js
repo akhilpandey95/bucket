0d3e3c5da149c2c69a654c93fd8248c822e909a3
AkhilHector <akhilhector.1@gmail.com>
2015-08-28 22:05:29 +0530

    Web.js now exports module router

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-var http = require('http')
-var express = require('express')
-var session = require('express-session')
-var bodyParser = require('body-parser')
-var errorHandler = require('errorhandler')
-var cookie = require('cookie-parser')
+var app = require('./web');
 
+app.router();
--- web.js
+++ web.js
@@ -3,70 +3,72 @@ var http = require('http'),
     express = require('express'),
     fs = require('fs'),
     bcrypt = require('bcrypt'),
-    errorPage = fs.readFileSync("./404.html"),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser');
 
-var port = process.env.PORT || 5000,
-    app = express(),
-    r = express.Router();
+module.exports.router = function() {
+		var port = process.env.PORT || 5000,
+	    app = express(),
+	    r = express.Router(),
+	    errorPage = fs.readFileSync("./404.html");
 
-require('node-jsx').install();
-app.use(express.static('assets'));
-app.set('title', "Bucket Sharing");
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+		require('node-jsx').install();
+		app.use(express.static('assets'));
+		app.set('title', "Bucket Sharing");
+		app.use(cookieParser());
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({extended: true}));
 
-r.get('/', function(req, res) {
-        var data = fs.readFileSync("index.html", "utf-8");
-        res.send(data.toString());
-});
+		r.get('/', function(req, res) {
+		        var data = fs.readFileSync("index.html", "utf-8");
+		        res.send(data.toString());
+		});
 
-r.get('/about', function(req, res) {
-        var data = fs.readFileSync("views/about.html", "utf-8");
-        res.send(data.toString());
-});
+		r.get('/about', function(req, res) {
+		        var data = fs.readFileSync("views/about.html", "utf-8");
+		        res.send(data.toString());
+		});
 
-r.get('/signup', function(req, res) {
-        var data = fs.readFileSync("views/signup.html", "utf-8");
-        res.send(data.toString());
-});
+		r.get('/signup', function(req, res) {
+		        var data = fs.readFileSync("views/signup.html", "utf-8");
+		        res.send(data.toString());
+		});
 
-r.post('/controller/login', function(req, res) {
-        var username = req.body.email;
-        var password = req.body.password;
-        var hash = bcrypt.compareSync(password, salt);
-        res.send(html);
-});
+		r.post('/controller/login', function(req, res) {
+		        var username = req.body.email;
+		        var password = req.body.password;
+		        var hash = bcrypt.compareSync(password, salt);
+		        res.send(html);
+		});
 
-r.post('/controller/signup', function(req, res) {
-        var username = req.body.username;
-        var email = req.body.email;
-        var pwd = req.body.password;
-        var confirm_pwd = req.body.password_confirm;
-        res.send(html);
-});
+		r.post('/controller/signup', function(req, res) {
+		        var username = req.body.username;
+		        var email = req.body.email;
+		        var pwd = req.body.password;
+		        var confirm_pwd = req.body.password_confirm;
+		        res.send(html);
+		});
 
-r.get('*', function(req, res) {
-        var match = 'views/' + req.params[0]+ ".html";
-        fs.exists(match, function(exists) {
-                if(exists) {
-                        fs.readFile(match, function(err, d) {
-                                if(err)
-                                   res.end(errorPage.toString(), 'utf-8');
-                                else
-                                   res.end(d, 'utf-8');
-                        });
-                }
-                else {
-                        res.end(errorPage.toString(), 'utf-8');
-                }
-        });
-});
+		r.get('*', function(req, res) {
+		        var match = 'views/' + req.params[0]+ ".html";
+		        fs.exists(match, function(exists) {
+		                if(exists) {
+		                        fs.readFile(match, function(err, d) {
+		                                if(err)
+		                                   res.end(errorPage.toString(), 'utf-8');
+		                                else
+		                                   res.end(d, 'utf-8');
+		                        });
+		                }
+		                else {
+		                        res.end(errorPage.toString(), 'utf-8');
+		                }
+		        });
+		});
 
-app.use('/', r);
+		app.use('/', r);
 
-http.createServer(app).listen(port, function() {
-        console.log("Listening on port 5000");
-})
+		http.createServer(app).listen(port, function() {
+		        console.log("Listening on port 5000");
+		});
+}
