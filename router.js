c6bde879e0b99fa8d928176c8a137e23a970e2d3
AkhilHector <akhilhector.1@gmail.com>
2016-08-27 15:50:31 +0530

    Initialising structure for version 0.0.2
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

---
+++ router.js
@@ -0,0 +1,11 @@
+/*
+ * Router for the application
+ * The MIT License
+ * Akhil Pandey
+ */
+
+const h = require('http'),
+	  e = require('express'),
+	  b = require('body-parser'),
+	  c = require('cookie-parser');
+
---
+++ scripts/string.js
@@ -0,0 +1,26 @@
+/*
+ * Helper mutator functions
+ * The MIT License
+ * Akhil Pandey
+ */
+
+/* Mutator function for generating the link
+ *
+ * @param {String} opt
+ *
+ */
+
+String..prototype.genUrl = function (opt) {
+	// combine
+}
+
+/* Mutator function for generating dictionary in order to support link generation
+ *
+ * @param {String} opt
+ *
+ */
+
+String..prototype.genDict = function (opt, callback) {
+	var an = Math.floor(Math.random()*10000000)
+	var al = "abcdefghijklmnopqrstuvwxyz"
+}
