133f7111770778f80b27208aecbdf3c4e6dbbe7f
AkhilHector <akhilhector.1@gmail.com>
2016-11-24 14:22:37 +0530

    Two way data binding done, links now are displayed
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- assets/controllers/linkCtrl.js
+++ assets/controllers/linkCtrl.js
@@ -1,5 +1,20 @@
 angular.module('bucket')
 
-.controller('linkCtrl', ['$http', function ($http) {
-  console.log('-----IGNORE------')
+.controller('linkCtrl', ['$http', '$scope',  function ($http, $scope) {
+    $scope.data = []
+    $scope.urls = []
+
+    $scope.submit = function() {
+        $scope.data.push($scope.enteredLink)
+        $scope.urls = [...new Set($scope.data)]
+    }
+
+    $scope.clear = function() {
+        $scope.enteredLink = ""
+    }
+
+    $scope.focusInput = function() {
+        document.getElementById("addMe").focus()
+    }
+
 }])
